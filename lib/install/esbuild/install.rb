381a0eb474919f3daa7b47d6a8d63c530d6e8819
张小辉 <xiaohui@tanmer.com>
2021-09-13 01:22:33 +0800

    add npm glob to devdependencies

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -1,6 +1,7 @@
 say "Install esbuild with config"
 copy_file "#{__dir__}/esbuild.config.js", "esbuild.config.js"
 run "yarn add esbuild"
+run "yarn add -D glob"
 say "Add stimulus autoloader"
 copy_file "#{__dir__}/stimulus_autoloader.js", "app/javascript/controllers/index.js"
 
