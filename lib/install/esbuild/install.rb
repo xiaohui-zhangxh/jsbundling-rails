b15025dcc20f664b2b0eb238915991afdbc7cb58
张小辉 <xiaohui@tanmer.com>
2021-09-13 01:05:34 +0800

    esbuild support glob import and autoload stimulus controllers

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -1,5 +1,8 @@
-say "Install esbuild"
+say "Install esbuild with config"
+copy_file "#{__dir__}/esbuild.config.js", "esbuild.config.js"
 run "yarn add esbuild"
+say "Add stimulus autoloader"
+copy_file "#{__dir__}/stimulus_autoloader.js", "app/javascript/controllers/index.js"
 
 say "Add build script"
-run %(npm set-script build "esbuild app/javascript/*.* --bundle --outdir=app/assets/builds")
+run %(npm set-script build "node esbuild.config.js")
